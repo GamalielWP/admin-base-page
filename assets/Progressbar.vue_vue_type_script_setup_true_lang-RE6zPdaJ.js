7d7d6accd195f2a09c98491ad4a4d27e74c81096
Gamaliel Widhi Pradana <board.dabil@gmail.com>
2025-02-09 09:35:43 +0700

    Initial commiy

---
+++ assets/Progressbar.vue_vue_type_script_setup_true_lang-RE6zPdaJ.js
@@ -0,0 +1,1 @@
+import{d as s,o as r,c as o,a,p as l}from"./index-CneCw9EU.js";const n={class:"w-full bg-gray-200 rounded-full"},d=s({__name:"Progressbar",props:{progress:{}},setup(t){return(e,p)=>(r(),o("div",n,[a("div",{class:"bg-purple-800 h-3 rounded-full transition-all duration-500",style:l({width:`${e.progress||0}%`})},null,4)]))}});export{d as _};
